fd0b6dd71824d7570f116bd52aba08c9a7853d24
marcosdcl <marcosdouglasclima@gmail.com>
2021-02-08 17:02:56 -0300

    Update models with max_digits

--- boteco_do_ralph/product/models.py
+++ boteco_do_ralph/product/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     description = models.CharField(max_length=255, null=True)
-    price = models.DecimalField(null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
