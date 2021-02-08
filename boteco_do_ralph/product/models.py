5a7fc56560e1c6c0f74be05f496ccbf1ce6a37ec
marcosdcl <marcosdouglasclima@gmail.com>
2021-02-08 16:55:25 -0300

    Bug fix

--- boteco_do_ralph/product/models.py
+++ boteco_do_ralph/product/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Product(models.Models):
+class Product(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     description = models.CharField(max_length=255, null=True)
     price = models.DecimalField(null=False)
